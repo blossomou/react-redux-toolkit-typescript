e3d9c33ee9c4de45fec507936e3040d6b6a5f5fe
QingOu <qingqingou@gmail.com>
2021-12-13 09:23:54 -0500

    initial setup ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="reservation-container">
+          <div>
+            <h5 className="reservation-header">Reservations</h5>
+            <div className="reservation-cards-container">
+              <div className="reservation-card-container">Laith Harb</div>
+            </div>
+          </div>
+          <div className="reservation-input-container">
+            <input />
+            <button>Add</button>
+          </div>
+        </div>
+        <div className="customer-food-container">
+          <div className="customer-food-card-container">
+            <p>Selena Gomez</p>
+            <div className="customer-foods-container">
+              <div className="customer-food"></div>
+              <div className="customer-food-input-container">
+                <input />
+                <button>Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
