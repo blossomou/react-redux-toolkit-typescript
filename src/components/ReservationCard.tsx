51a8df27cf5646de87eede563de79a02c18dede0
QingOu <qingqingou@gmail.com>
2021-12-13 10:44:36 -0500

    addCustomer action

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 
+import { addCustomer } from '../features/customerSlice';
 import { removeReservation } from '../features/reservationSlice';
 
 interface ReservationCardTypes {
@@ -9,10 +11,18 @@ interface ReservationCardTypes {
 }
 export default function ReservationCard({ name, index }: ReservationCardTypes) {
   const dispatch = useDispatch();
+
   return (
     <div
       onClick={() => {
         dispatch(removeReservation(index));
+        dispatch(
+          addCustomer({
+            id: uuid(),
+            name: name,
+            food: [],
+          })
+        );
       }}
       className="reservation-card-container"
     >
--- src/features/customerSlice.ts
+++ src/features/customerSlice.ts
@@ -1,23 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CustomerState{
 
     value: Customer[]
 }
-
 interface Customer{
     id: string;
     name: string;
     food: string[];
 }
+
 const initialState : CustomerState = {
     value: []
 }
 export const customersSlice = createSlice({
     name: "customers",
     initialState: initialState,
-    reducers:{}
+    reducers:{
+        addCustomer: (state, action: PayloadAction<Customer>) => {
+            state.value.push(action.payload)
+        }
+    }
 })
 
-
+export const {addCustomer} = customersSlice.actions;
 export default customersSlice.reducer;
