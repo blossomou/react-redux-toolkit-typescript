14f922b0c08795dd96a993b9ae55d74ab5910f6b
QingOu <qingqingou@gmail.com>
2021-12-13 10:33:57 -0500

    create CustomerCard Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { RootState } from './app/store';
+import CustomerCard from './components/CustomerCard';
 import ReservationCard from './components/ReservationCard';
 import { addReservation } from './features/reservationSlice';
 
 function App() {
   const [reservationNameInput, setReservationNameInput] = useState("");
   const reservations = useSelector((state: RootState) => state.reservations.value);
+  const customers = useSelector((state: RootState) => state.customers.value);
 
   const dispatch = useDispatch();
   const handleAddReservations = () => {
@@ -35,16 +37,9 @@ function App() {
           </div>
         </div>
         <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
-              </div>
-            </div>
-          </div>
+          {customers.map((customer) => {
+            return <CustomerCard />;
+          })}
         </div>
       </div>
     </div>
