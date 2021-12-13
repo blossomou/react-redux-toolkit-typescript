99cb9e54ee05ae8ec9865ce9475636c4fd2db5e3
QingOu <qingqingou@gmail.com>
2021-12-13 09:50:35 -0500

    reservationSlice payload

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,14 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initalState = {
+interface ReservationState{
+    value: string[]
+}
+const initalState: ReservationState = {
     value: []
 
 }
 export const reservationsSlice = createSlice({
     name: "reservations",
     initialState: initalState,
-    reducers: {}
+    reducers: {
+        addReservation: (state, action: PayloadAction<string>) =>{
+            state.value.push(action.payload)
+        }
+    }
     
 })
+
+export const { addReservation } = reservationsSlice.actions;
 
 export default reservationsSlice.reducer;
